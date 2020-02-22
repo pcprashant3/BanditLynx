e40e920b0a62c0e480664d5862f5b1ae69d10fb9
pcprashant3 <pcprashant3@gmail.com>
2020-02-22 01:22:52 -0600

    Create arraych.c

---
+++ arraych.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+  char words[]= {'p', 'r' , 'a', 's', 'h', 'a', 'n', 't', 'c', 'h'};
+
+    printf("\nElements in array are: ");  
+    for(int i=0; i<5; i++)  
+    {  
+        printf("%c\n", words[i]);  
+    } 
+    printf("\n");	
+
+return 0;
+}
